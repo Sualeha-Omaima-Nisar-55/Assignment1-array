57863b3c3941ae42b516c7c55020f5f8fc1a0229
Sualeha-Omaima-Nisar-55 <nazifarooqui2850@gmail.com>
2026-03-17 00:05:20 -0700

    Fix formatting and logic in string.java

--- string.java
+++ string.java
@@ -7,13 +7,13 @@ class string {
        if(a%2==0){
         System.out.println("It's an even number");}
        else{
-        System.out.println("It's odd number");}
+        System.out.println("It's an odd number");}
         System.out.print("Enter second(b) number:");
        int b= sc.nextInt();
         if(a%2==0){
         System.out.println("It's an even number");}
        else{
-        System.out.println("It's odd number");
+        System.out.println("It's an odd number");
          int sum= a+b;
          System.out.print("Total sum of 2 numbers is: "+ sum +"");
     }
